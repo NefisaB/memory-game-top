1b8c80696db9dd878417cdbf815e6a9e9f87859b
NefisaB <nefisa.hc@gmail.com>
2023-02-10 09:49:03 +0100

    another images fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ export default function Card({ pokemon, handleCardClick }) {
         <div
             className="card"
             onClick={() => handleCardClick(pokemon)}>
-            <img src={`${process.env.PUBLIC_URL}/images/${pokemon}.png`} alt={pokemon} />
+            <img src={`./images/${pokemon}.png`} alt={pokemon} />
             <h2>{pokemon}</h2>
         </div>
     );
